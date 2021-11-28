2a9b19793c6862dc3c3704bd52f308cc2ee46e72
ppond454 <ppond.454@gmail.com>
2021-11-28 19:12:25 +0700

    make it better

--- src/server.js
+++ src/server.js
@@ -1,27 +1,13 @@
-// import express from "express";
-// import Project from "./route/project"
-// import Projectpage from "./route/projectpage"
-// import cors from "cors"
-const cors = require("cors")
-const express = require("express")
+import express from "express";
+import Project from "./route/project"
+import Projectpage from "./route/projectpage"
+import cors from "cors"
 
 const app = express()
-
 app.use(cors())
 
-
-
-// app.use(Project)
-// app.use(Projectpage)
-
-app.get("/" ,(req ,res)=>{
-
-    return res.status(200).json({
-    hello : "ok"
-    })
-
-
-})
+app.use(Project)
+app.use(Projectpage)
 
 
 app.listen(process.env.PORT || 4000, function(){
