0750411547d19960eea63b49c7b468f1a1475d81
ppond454 <ppond.454@gmail.com>
2021-11-28 18:03:39 +0700

    /

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-import express from "express";
-import Project from "./route/project"
-import Projectpage from "./route/projectpage"
-import cors from "cors"
+// import express from "express";
+// import Project from "./route/project"
+// import Projectpage from "./route/projectpage"
+// import cors from "cors"
+const express = require("express")
+const app = express()
 
 let PORT = process.env.PORT || 4000
 
-require('dotenv').config()
-
-const app = express()
-
-app.use(cors())
-app.use(express.static("public"))
 
 
-app.use(Project)
-app.use(Projectpage)
+app.get("/", (req ,res)=>{
+    res.status(200).json({
+        message : "ok"
+    })
+})
+
+app.get("/:id", (req ,res)=>{
+    
+    let {id} = req.params
+    res.status(200).json({
+        message : id
+    })
+})
+
 
 
 app.listen(PORT,()=>{
